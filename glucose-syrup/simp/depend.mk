dee00e5b99d2214178ecb0e3cf918169cc30657b
SWGAKamui <kamui_swga@outlook.fr>
2016-11-24 23:37:08 +0100

    first commit

---
+++ glucose-syrup/simp/depend.mk
@@ -0,0 +1,38 @@
+/home/kinda/Documents/DM/glucose-syrup/simp/SimpSolver.o /home/kinda/Documents/DM/glucose-syrup/simp/SimpSolver.or /home/kinda/Documents/DM/glucose-syrup/simp/SimpSolver.od /home/kinda/Documents/DM/glucose-syrup/simp/SimpSolver.op: /home/kinda/Documents/DM/glucose-syrup/simp/SimpSolver.cc \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/Sort.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/Vec.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/IntTypes.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/XAlloc.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../simp/SimpSolver.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/Queue.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../core/Solver.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/Heap.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/Alg.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../utils/Options.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../utils/ParseUtils.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../core/SolverTypes.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/Map.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/Alloc.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../core/BoundedQueue.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../core/Constants.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/Clone.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../utils/System.h
+/home/kinda/Documents/DM/glucose-syrup/simp/Main.o /home/kinda/Documents/DM/glucose-syrup/simp/Main.or /home/kinda/Documents/DM/glucose-syrup/simp/Main.od /home/kinda/Documents/DM/glucose-syrup/simp/Main.op: /home/kinda/Documents/DM/glucose-syrup/simp/Main.cc \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../utils/System.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/IntTypes.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../utils/ParseUtils.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../utils/Options.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/Vec.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/XAlloc.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../core/Dimacs.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../core/SolverTypes.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/Alg.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/Map.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/Alloc.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../simp/SimpSolver.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/Queue.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../core/Solver.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/Heap.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../core/BoundedQueue.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../core/Constants.h \
+ /home/kinda/Documents/DM/glucose-syrup/simp/../mtl/Clone.h
